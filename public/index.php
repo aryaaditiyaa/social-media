8f844c355e82fb03103f2e5d552a60e48ee54f91
Arya Aditiya <aryaaditiya15@gmail.com>
2020-10-26 12:29:40 +0700

    selesai video 1

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php require_once("layouts/header.php") ?>
+<?php require_once("layouts/navbar.php") ?>
+
+<div class="container">
+    <div class="row">
+        <div class="row">
+            <h1>aaaaaaaaaaaaaaa</h1>
+        </div>
+    </div>
+</div>
+
+<?php require_once("layouts/footer.php") ?>
---
+++ public/layouts/navbar.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top py-3">
+    <div class="container">
+        <a class="navbar-brand" href="http://localhost/paw-tugas-uts/public">Social Media</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+                aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="">
+                        <i class='fas fa-home mr-1'></i>
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">
+                        <i class='fas fa-user mr-1'></i>
+                        User
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">
+                        <i class='fas fa-bullhorn mr-1'></i>
+                        Post
+                    </a>
+                </li>
+            </ul>
+            <div class="navbar-nav">
+                <a href=""
+                   class="btn btn-outline-success my-2 my-sm-0">Login</a>
+                <a href=""
+                   class="btn btn-success my-2 my-sm-0 ml-2">Register</a>
+            </div>
+        </div>
+    </div>
+</nav>
